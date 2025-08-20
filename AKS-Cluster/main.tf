69dba560231a23fc43f0344e352415abcfd0a1ec
Dennis Tei-Muno <munodennis@gmail.com>
2025-08-20 03:06:49 -0500

    aks folder for terraform deployment

---
+++ AKS-Cluster/main.tf
@@ -0,0 +1,30 @@
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "example" {
+  name     = "kubernetes-cluster-multicloud-chatbotg"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks1"
+  depends_on = [ azurerm_resource_group.example ]
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
